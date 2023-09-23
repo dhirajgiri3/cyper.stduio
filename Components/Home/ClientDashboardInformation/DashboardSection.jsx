cda9ecf6de7822d9c79a7f7a09e35492086baf10
dhiraj giri <2021356392.giri@ug.sharda.ac.in>
2023-09-23 21:48:44 +0530

    'vid'

--- Components/Home/ClientDashboardInformation/DashboardSection.jsx
+++ Components/Home/ClientDashboardInformation/DashboardSection.jsx
@@ -222,7 +222,8 @@ const DashboardSection = () => {
     <DashboardSectionContainer>
       <video autoPlay loop muted playsInline className="introvid">
         <source
-          src="https://res.cloudinary.com/divbobkmd/video/upload/v1695482394/intro-cyper_ru9cpz.mp4"
+          // src="https://res.cloudinary.com/divbobkmd/video/upload/v1695482394/intro-cyper_ru9cpz.mp4"
+          src="https://res.cloudinary.com/divbobkmd/video/upload/v1695425223/Cyper%20studio/yes-oh_mnadqn.mp4"
           type="video/mp4"
         />
       </video>
