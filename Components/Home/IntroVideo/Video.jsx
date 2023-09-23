78b00d9786a77da6c4b255cb5f854de163ca0a2e
dhiraj giri <2021356392.giri@ug.sharda.ac.in>
2023-09-23 16:52:35 +0530

    'vid6'

--- Components/Home/IntroVideo/Video.jsx
+++ Components/Home/IntroVideo/Video.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const VideoContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 3rem 0;
+
+  .introvid {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+
+    @media screen and (max-width: 1024px) {
+      height: 30vh;
+    }
+
+    @media screen and (max-width: 768px) {
+      height: 40vh;
+    }
+  }
+`;
 
 function Video() {
   return (
-    <div>Video</div>
-  )
+    <VideoContainer>
+      <video
+        autoPlay
+        loop
+        muted
+        className="introvid"
+        src="https://res.cloudinary.com/divbobkmd/video/upload/v1695467978/Cyper%20studio/cyper-intro_bfs06k.mp4"
+      ></video>
+    </VideoContainer>
+  );
 }
 
-export default Video
+export default Video;
