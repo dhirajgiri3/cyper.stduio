accfce2b08b14bf69d7f5248181bfb87f38b675a
dhiraj giri <2021356392.giri@ug.sharda.ac.in>
2023-09-23 22:45:06 +0530

    r

--- Components/Home/FirstSection/FirstSection.jsx
+++ Components/Home/FirstSection/FirstSection.jsx
@@ -175,8 +175,8 @@ function FirstSection() {
         <motion.div variants={fadeVariants} className="bgvideo">
           <video autoPlay loop muted playsInline>
             <source
-              src="https://res.cloudinary.com/divbobkmd/video/upload/v1695302345/cube-loop_pwawki.webm"
-              type="video/webm"
+              src="https://res.cloudinary.com/divbobkmd/video/upload/v1695487376/Cyper%20studio/cube-loop-comp_em4pjt.mp4"
+              type="video/mp4"
             />
           </video>
         </motion.div>
